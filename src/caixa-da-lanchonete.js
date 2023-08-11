a75e43ccffaaaac0df35fd1ea36a3dce8962d485
liziane <lizianecysne1@gmail.com>
2023-08-11 14:53:50 +0000

    fix: corrige validação do debito, corrige a logica dos itens extras e corrige '.' e ',' que estavam sendo trocados no resultado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,49 +1,62 @@
 class CaixaDaLanchonete {
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        const cardapio = {
-            cafe: 3,
-            chantily: 1.5,
-            suco: 6.2,
-            sanduiche: 6.5,
-            queijo: 2,
-            salgado: 7.25,
-            combo1: 9.5,
-            combo2: 7.5
-        };
+  calcularValorDaCompra(formaDePagamento, itens) {
+      const cardapio = {
+          cafe: 3,
+          chantily: 1.5,
+          suco: 6.2,
+          sanduiche: 6.5,
+          queijo: 2,
+          salgado: 7.25,
+          combo1: 9.5,
+          combo2: 7.5
+      };
 
-        if (itens.length === 0) {
-            return "Não há itens no carrinho de compra!";
-        }
+      if (itens.length === 0) {
+          return "Não há itens no carrinho de compra!";
+      }
+      
+      let total = 0;
+      let hasCafe = false;
+      let hasSanduiche = false;
 
-        let total = 0;
+      for (const itemInfo of itens) {
+          const [item, quantidade] = itemInfo.split(",");
 
-        for (const itemInfo of itens) {
-            const [item, quantidade] = itemInfo.split(",");
+          // Add this logic to check for extra items
+          if(item === "chantily" && !hasCafe || item === "queijo" && !hasSanduiche) {
+              return "Item extra não pode ser pedido sem o principal"
+          }
 
-            if (!cardapio.hasOwnProperty(item)) {
-                return "Item inválido!";
-            }
+          if (!cardapio.hasOwnProperty(item)) {
+              return "Item inválido!";
+          }
 
-            const precoItem = cardapio[item];
+          const precoItem = cardapio[item];
 
-            if (quantidade <= 0) {
-                return "Quantidade inválida!";
-            }
+          if (quantidade <= 0) {
+              return "Quantidade inválida!";
+          }
 
-            total += precoItem * quantidade;
-        }
+          // Update flags for main items
+          if(item == "cafe") hasCafe = true;
+          if(item == "sanduiche") hasSanduiche = true;
 
-        if (metodoDePagamento === "dinheiro") {
-            total = total * 0.95;
-        } else if (metodoDePagamento === "credito") {
-            total = total * 1.03;
-        } else {
-            return "Forma de pagamento inválida!";
-        }
+          total += precoItem * quantidade;
+      }
 
-        return `R$ ${total.toFixed(2)}`;
-    }
+      if (formaDePagamento === "dinheiro") {
+          total = total * 0.95;
+      } else if (formaDePagamento === "credito") {
+          total = total * 1.03;
+      } else if (formaDePagamento === "debito") {
+          total = total;
+      } else {
+          return "Forma de pagamento inválida!";
+      }
+
+      return `R$ ${total.toFixed(2).toString().replace(".", ",")}`;
+  }
 
 }
 
